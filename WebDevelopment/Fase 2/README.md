06e08cce8a8cff816da9fde4354ae1b6774d4fa0
Martin Molema <martin@molema.org>
2018-11-11 13:49:41 +0100

    Work in progress on first phases

--- WebDevelopment/Fase 2/README.md
+++ WebDevelopment/Fase 2/README.md
@@ -4,4 +4,19 @@ een los bestand. Ook de stylesheet is verhuisd naar een los bestand. Typisch zet
 je deze in een eigen map (js en css).
 
 Probleem is wel dat je niet precies weet wanneer je HTML klaar is om te
-gebruiken in Javascript
+gebruiken in Javascript. Daarom gebruiken we de functie
+
+```javascript
+window.onload
+```
+
+Hier kun je een functie (anoniem of functienaam) aan toekennen die uitgevoerd wordt als de
+HTML klaar is met laden ([Link naar MDN](https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onload)).
+
+Tevens gaan we aan de slag met het concept van de DOM : het "Document Object Model".
+
+Referenties:
+  * [GetElementByID](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById)
+  * [CreateElement](https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement)
+  * [documentElement](https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement)
+  
--- WebDevelopment/Fase 2/js/index.js
+++ WebDevelopment/Fase 2/js/index.js
@@ -1,13 +1,29 @@
-window.onload = function(){
+window.onload = function(){ //* Anonymous function called when all HTML objects are loaded in the DOM
     initPage();
-}
+}//window.onload
 
+/**
+ * Function called to initialise the page
+ */
 function initPage(){
-    var container = document.getElementById("container");
-    container.textContent = "Hello World (Fase 2)";
+    // search for the container using and ID-selector and store in JS variable "container"
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+    var container = document.getElementById("container1");
 
+    // modify the content of the container to simple text content
+    // only execute the code if the container is not null (not found)
+    if (container) container.textContent = "Hello World (Fase 2)";
+
+    // now create a new P-element and add it to the end of the current document ("DOM tree body")
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
     var newDiv = document.createElement("P");
+    // set classname
     newDiv.className = 'welkom';
+
+    // set content of the newly created element
     newDiv.innerHTML = "<span>Hello</span>World";
+
+    // add it to the DOM
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement
     document.documentElement.appendChild(newDiv);
 }
