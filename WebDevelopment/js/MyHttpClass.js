7734ad713c67293146191cf04185f3d586db9b09
Martin Molema <martin@molema.org>
2018-11-18 16:03:03 +0100

    fixed copy-paste from other MyHTTP class  - added cache prevention headers

--- WebDevelopment/js/MyHttpClass.js
+++ WebDevelopment/js/MyHttpClass.js
@@ -1,18 +1,25 @@
 export class MyHttp {
-    getDataFromIMDB(url){
-        console.log(">>getDataFromIMDB");
+    getDataFromURL(url){
+        console.log(">>getDataFromURL");
 
         // create a promise that will be returned as the function's result
         // the calling function can supply the resolve and reject function.
 
         var prom = new Promise(function(resolve, reject) {
-            console.log("getDataFromIMDB::Promise body");
+            console.log("getDataFromURL::Promise body");
 
             // create an object to call a REST API
             var xhr = new XMLHttpRequest();
 
             // set the URL to be opened
             xhr.open('GET', url);
+
+            // set (a lot of) options to prevent caches pages
+            xhr.setRequestHeader('cache-control', 'no-cache, must-revalidate, post-check=0, pre-check=0');
+            xhr.setRequestHeader('cache-control', 'max-age=0');
+            xhr.setRequestHeader('expires', '0');
+            xhr.setRequestHeader('expires', 'Tue, 01 Jan 1980 1:00:00 GMT');
+            xhr.setRequestHeader('pragma', 'no-cache');
 
             // set a function that is called when the API call is completed without errors
             xhr.onload = function () {
@@ -32,8 +39,8 @@ export class MyHttp {
             xhr.send();
 
         });
-        console.log("<<getDataFromIMDB");
+        console.log("<<getDataFromURL");
         return prom;
-    }//getDataFromIMDB
+    }//getDataFromURL
 
 }//class MyHttpClass
