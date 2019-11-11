bcd73fac37d63eaaf009e1a7b56a8b4d5c4193e3
Martin Molema (Docent NHL Stenden) <martin.molema@nhl.nl>
2019-11-11 09:20:51 +0100

    Bestanden verplaatst in de 'Examples' sectie, en titels tabbladen aangepast.

--- WebDevelopment/js/examples.js
+++ WebDevelopment/js/examples.js
@@ -2,28 +2,22 @@ import {MarkdownSupporter} from "./MarkdownSupporter.js";
 
 $( function() {
     var examples = [
-        {dir:"Fase01", caption:"Fase 1"},
-        {dir:"Fase02", caption:"Fase 2"},
-        {dir:"Fase03", caption:"Fase 3"},
-        {dir:"Fase04", caption:"Fase 4"},
-        {dir:"Fase05", caption:"Fase 5"},
-        {dir:"Fase06", caption:"Fase 6"},
-        {dir:"Fase07", caption:"Fase 7"},
-        {dir:"Fase08", caption:"Fase 8"},
-        {dir:"Fase09", caption:"Fase 9"},
+        {dir:"Fase01", caption:"1. Javascript start"},
+        {dir:"Fase02", caption:"2. DOM"},
+        {dir:"Fase03", caption:"3. JQuery 1"},
+        {dir:"Fase04", caption:"4. JQuery 2"},
+        {dir:"Fase05", caption:"5. Asynchroon Promises"},
+        {dir:"Fase06", caption:"6. Promises 2"},
+        {dir:"Fase07", caption:"7. Boostrap"},
+        {dir:"Fase08", caption:"8. Animaties"},
+        {dir:"Fase09", caption:"9. "},
         {dir:"Fase10", caption:"Fase 10"},
         {dir:"Fase11", caption:"Fase 11"},
-        {dir:"Fase12", caption:"Fase 12"},
-        {dir:"Fase13", caption:"Fase 13"},
-        {dir:"Fase14", caption:"Fase 14"},
-        {dir:"Fase15", caption:"Fase 15"},
-        {dir:"Fase16", caption:"Fase 16"},
-        {dir:"Fase17", caption:"Fase 17"},
-        {dir:"Fase18", caption:"Fase 18"},
-        {dir:"Fase19", caption:"Fase 19"},
-        {dir:"Fase10", caption:"Fase 10"},
-        {dir:"Fase11", caption:"Fase 11"},
-        {dir:"Fase12", caption:"Fase 12"}
+        {dir:"MyFirstSimpleApp01", caption:"Angular 1"},
+        {dir:"MyFirstSimpleApp02", caption:"Angular 2"},
+        {dir:"MyFirstAngularApp", caption:"Angular 3"},
+        {dir:"MyFirstAngularAppV2", caption:"Angular 4"},
+        {dir:"MyFirstDataEntry", caption:"Angular Data Entry"},
     ];
 
     var mdSupporter = new MarkdownSupporter("content/examples");
