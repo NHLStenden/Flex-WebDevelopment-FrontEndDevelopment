2b6b666d07379b35a5bd264551b2bd59461582ca
Martin Molema (NHL) <martin.molema@nhlstenden.com>
2020-11-30 18:18:35 +0100

    Nieuwe presentatie en coding voorbeelden

---
+++ WebDevelopment/content/temporary/LiveCoding/voorbeelden/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'voorbeelden';
+  public fruit = ["Appel", "Banaan", "Ananas", "Mandarijn", "Sinaasappel", "Peer"];
+
+  
+}
