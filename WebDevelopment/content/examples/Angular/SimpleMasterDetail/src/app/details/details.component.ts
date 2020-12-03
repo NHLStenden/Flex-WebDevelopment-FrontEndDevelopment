6c187d5546c4fe41750e01be735b945f23885bb7
Martin Molema (NHL) <martin.molema@nhlstenden.com>
2020-12-03 17:18:49 +0100

    Added some comments

--- WebDevelopment/content/examples/Angular/SimpleMasterDetail/src/app/details/details.component.ts
+++ WebDevelopment/content/examples/Angular/SimpleMasterDetail/src/app/details/details.component.ts
@@ -17,11 +17,17 @@ export class DetailsComponent implements OnInit {
   constructor(private store: MasterDetailService) { }
 
   ngOnInit(): void {
+	// we luiisteren naar de observable in de STORE voor het HTML-sjabloon	  
     this.selectedItem$ = this.store.selectedItem$;
+	
+	// we luisteren naar de observable in STORE om een verwijzing te krijgen naar het geselecteerde object
+	// ZODAT: we in de ageUp() en ageDown() functie kunnen meegeven welk object (Person) we willen veranderen
     this.selectedItem$.subscribe(newSelectedItem => this.selectedItem = newSelectedItem);
   }
 
   public ageUp(): void {
+	// dit component weet dus niet wat er voor nodig is om de leeftijd te verhogen, of hoe dat werkt.
+	// door te luisteren naar de observable in de store, krijgen we uiteindelijk de nieuwe informatie weer terug.
     this.store.increaseAge(this.selectedItem);
   }
 
