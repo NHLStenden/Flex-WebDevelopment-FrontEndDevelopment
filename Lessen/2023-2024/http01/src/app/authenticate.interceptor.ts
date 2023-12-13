298ec0d80365405db335d1adccc5de8ecbfcffac
Martin Molema <martin.molema@nhlstenden.com>
2023-12-13 12:27:19 +0100

    Removed bearer token

--- Lessen/2023-2024/http01/src/app/authenticate.interceptor.ts
+++ Lessen/2023-2024/http01/src/app/authenticate.interceptor.ts
@@ -13,7 +13,7 @@ export class AuthenticateInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    request.headers.set('Authorization', 'bearer eyJhbGciOiJTSEEyNTYiLCJ0eXAiOiJKV1QifQ.eyJub3VuIjoidXNlclwvYXV0aGVudGljYXRlIiwicmVxdWVzdF90eXBlIjoiUE9TVCIsInJlcXVlc3RlZF9mb3IiOiJodHRwczpcL1wvdmlld2VyLmNyaXNpc2dhbWUubmwiLCJ1c2VyIjoidXNlclwvMTQiLCJuYW1lIjoiVmlld2VyIDAyIiwicmVtb3RlIjoiMTQxLjI1Mi4yMDguMTI2Iiwicm9sZXMiOlsidmlld2VyIl0sImV4cCI6MTcwMTcwNDAzNSwianRpIjoiZDM2MmVmNjA4NTc2NDE3Y2ZjZTc0MDE0MWExMTMxYTQiLCJpYXQiOjE3MDE2ODk2MzUsImlzcyI6IkNyaXNpc2dhbWVBUEkifQ.G6Ican64uDyAYzLP0-X157ehC77HtYYeRaTatqLJx5o')
+    request.headers.set('Authorization', 'bearer 12345')
     return next.handle(request);
   }
 }
